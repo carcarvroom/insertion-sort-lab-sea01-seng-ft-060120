65711b261e3cd2db0fc99507f510dbfce9ec4de0
carcarvroom <carly_l@outlook.com>
2020-09-17 10:28:02 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+  let min = array[0]
+  let index = 0
+  for (let i = 1; i < array.length; i++) {
+    if(array[i] < min) {
+      min = array[i]
+      index = i
+    }
+  }
+  array.splice(index, 1)
+  return min
 }
 
 function selectionSort(array){
-
+  let arrayMin
+  let sortedArray = []
+  while (array.length !== 0) {
+    arrayMin = findMinAndRemove(array)
+    sortedArray.push(arrayMin)
+  }
+  return sortedArray
 }
